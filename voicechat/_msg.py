60c5f9b4af4278ba10de0388dfd2c0c0d67a6e8e
Tao Xiang <xiang.tao@outlook.de>
2023-03-02 11:42:27 +0100

    1

---
+++ voicechat/_msg.py
@@ -0,0 +1,48 @@
+import flet as ft
+
+class Message():
+    def __init__(self, user_name: str, text: str, message_type: str):
+        self.user_name = user_name
+        self.text = text
+        self.message_type = message_type
+
+class ChatMessage(ft.Row):
+    def __init__(self, message: Message):
+        super().__init__()
+        self.vertical_alignment="start"
+        self.controls=[
+                ft.CircleAvatar(
+                    content=ft.Text(self.get_initials(message.user_name)),
+                    color=ft.colors.WHITE,
+                    bgcolor=self.get_avatar_color(message.user_name),
+                ),
+                ft.Column(
+                    [
+                        ft.Text(message.user_name, weight="bold"),
+                        ft.Text(message.text, selectable=True),
+                    ],
+                    tight=True,
+                    spacing=5,
+                ),
+            ]
+
+    def get_initials(self, user_name: str):
+        return user_name[:1].capitalize()
+
+    def get_avatar_color(self, user_name: str):
+        colors_lookup = [
+            ft.colors.AMBER,
+            ft.colors.BLUE,
+            ft.colors.BROWN,
+            ft.colors.CYAN,
+            ft.colors.GREEN,
+            ft.colors.INDIGO,
+            ft.colors.LIME,
+            ft.colors.ORANGE,
+            ft.colors.PINK,
+            ft.colors.PURPLE,
+            ft.colors.RED,
+            ft.colors.TEAL,
+            ft.colors.YELLOW,
+        ]
+        return colors_lookup[hash(user_name) % len(colors_lookup)]
--- voicechat/chat.py
+++ voicechat/chat.py
@@ -1,51 +1,5 @@
 import flet as ft
-
-class Message():
-    def __init__(self, user_name: str, text: str, message_type: str):
-        self.user_name = user_name
-        self.text = text
-        self.message_type = message_type
-
-class ChatMessage(ft.Row):
-    def __init__(self, message: Message):
-        super().__init__()
-        self.vertical_alignment="start"
-        self.controls=[
-                ft.CircleAvatar(
-                    content=ft.Text(self.get_initials(message.user_name)),
-                    color=ft.colors.WHITE,
-                    bgcolor=self.get_avatar_color(message.user_name),
-                ),
-                ft.Column(
-                    [
-                        ft.Text(message.user_name, weight="bold"),
-                        ft.Text(message.text, selectable=True),
-                    ],
-                    tight=True,
-                    spacing=5,
-                ),
-            ]
-
-    def get_initials(self, user_name: str):
-        return user_name[:1].capitalize()
-
-    def get_avatar_color(self, user_name: str):
-        colors_lookup = [
-            ft.colors.AMBER,
-            ft.colors.BLUE,
-            ft.colors.BROWN,
-            ft.colors.CYAN,
-            ft.colors.GREEN,
-            ft.colors.INDIGO,
-            ft.colors.LIME,
-            ft.colors.ORANGE,
-            ft.colors.PINK,
-            ft.colors.PURPLE,
-            ft.colors.RED,
-            ft.colors.TEAL,
-            ft.colors.YELLOW,
-        ]
-        return colors_lookup[hash(user_name) % len(colors_lookup)]
+from ._msg import Message,ChatMessage
 
 def main(page: ft.Page):
     page.horizontal_alignment = "stretch"
@@ -59,15 +13,17 @@ def main(page: ft.Page):
             page.session.set("user_name", join_user_name.value)
             page.dialog.open = False
             new_message.prefix = ft.Text(f"{join_user_name.value}: ")
-            page.pubsub.send_all(Message(user_name=join_user_name.value, text=f"{join_user_name.value} has joined the chat.", message_type="login_message"))
+            # page.pubsub.send_all(Message(user_name=join_user_name.value, text=f"{join_user_name.value} has joined the chat.", message_type="login_message"))
             page.update()
 
     def send_message_click(e):
         if new_message.value != "":
-            page.pubsub.send_all(Message(page.session.get("user_name"), new_message.value, message_type="chat_message"))
+            # page.pubsub.send_all(Message(page.session.get("user_name"), new_message.value, message_type="chat_message"))
+            on_message(Message(page.session.get("user_name"), new_message.value, message_type="chat_message"))
             new_message.value = ""
             new_message.focus()
             page.update()
+
 
     def on_message(message: Message):
         if message.message_type == "chat_message":
@@ -77,7 +33,7 @@ def main(page: ft.Page):
         chat.controls.append(m)
         page.update()
 
-    page.pubsub.subscribe(on_message)
+    # page.pubsub.subscribe(on_message)
 
     # A dialog asking for a user display name
     join_user_name = ft.TextField(
