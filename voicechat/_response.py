21e9bba11fe5f9b8ce08edc55abbbd853deddf23
Tao Xiang <xiang.tao@outlook.de>
2023-03-02 12:39:53 +0100

    1

--- voicechat/_response.py
+++ voicechat/_response.py
@@ -1,3 +1,13 @@
+import openai
+openai.api_key='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'
+
+
 def reply(msg:str):
-    return msg
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": f"{msg}, 請用粵語答我."}]
+    )
+    response = completion['choices'][0]['message']['content'].strip()
+    print(response)
+    return response
 
